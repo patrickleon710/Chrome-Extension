76a8b3e9e280529a5f58ecab15042442b59816a3
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-08 16:24:22 +0300

    final touches, functional app

--- index.js
+++ index.js
@@ -4,24 +4,31 @@ let myLeads = []
 let inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
 let inputBtn = document.getElementById("input-btn")
+let tabBtn = document.getElementById("tab-btn")
 let deleteBtn = document.getElementById("delete-btn")
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
 // use parameters
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    render()
+    myLeads.push(inputEl.value)    
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
-function render() {
+function render(arr) {
     let listContents = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         listContents += `
             <li>
-                <a target="_blank" href="${myLeads[i]}"> 
-                    ${myLeads[i]} 
+                <a target="_blank" href="${arr[i]}"> 
+                    ${arr[i]} 
                 </a>
             </li>
         `
@@ -29,7 +36,17 @@ function render() {
     ulEl.innerHTML = listContents
 }
 
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
 deleteBtn.addEventListener("dblclick", function() {
-    
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
     
 })
